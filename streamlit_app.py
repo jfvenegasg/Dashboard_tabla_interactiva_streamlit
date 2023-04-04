c3a8203075c2374beaec828a88aeb1dfba8ff01c
Juan Venegas <j.venegasgutierrez@gmail.com>
2023-04-04 02:00:49 -0400

    se actualiza dash.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,25 +5,75 @@ from st_aggrid import AgGrid, GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode
 from streamlit_option_menu import option_menu
 import streamlit_nested_layout as stn
+import plotly.express as px
+from streamlit_elements import elements, mui, html
+
 st.set_page_config(layout = "wide")
 # 1. as sidebar menu
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Inicio","Visualizacion", 'Configuracion'], 
-        icons=['house', 'stack','gear'], menu_icon="cast", default_index=1)
+    selected = option_menu("Sistema de gestión HCM", ["Inicio","Reporte quirófanos", "Suspensiones por causa","Suspensiones por especialidad","Hospitalización domiciliaria","Días de estadia"], 
+        icons=['house', 'person-rolodex','person-badge-fill','person-badge-fill','file-earmark-medical','file-earmark-medical'], menu_icon="cast", default_index=0)
     selected
 
 if selected == "Inicio":
      
  
-    col1,col2=st.columns(2,gap="small")
     
-    with col1:
-
-        STREAMLIT_AGGRID_URL = "https://github.com/PablocFonseca/streamlit-aggrid"
         #st.set_page_config(
         #    layout="centered", page_icon="🖱️", page_title="Interactive table app"   
         #)
-        st.title("🖱️ Aplicacion de Tabla Interactiva")
+    c=st.empty()
+        
+    col_inicio_1,col_inicio_2=st.columns(2,gap="small")
+        
+    c.image("hcm.png")
+    with col_inicio_1:
+            st.button(label="Reporte quirofanos")
+            st.button(label="Suspensiones por causa")
+            st.button(label="Suspensiones por especialidad")
+    with col_inicio_2:
+            st.button(label="Hospitalización domiciliaria")
+            st.button(label="Dias de estadia")
+    
+elif selected =="Reporte quirófanos":
+    option = {
+            "xAxis": {
+                "type": "category",
+                "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            },
+            "yAxis": {"type": "value"},
+            "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320],
+                        "type": "line"}],
+             "title": ["titulo"]}
+    iris = pd.read_excel("datos_suspensiones_sankey_bd.xlsx")
+    
+    fig = px.bar(
+    iris,
+    y="target",
+    x="value",
+    height=700,
+    orientation="h")
+    fig.update_layout(title="GDP per Capita vs. Life Expectancy")
+    
+    col_report_1,col_report_2=st.columns([4,1],gap="small")  
+    
+    with col_report_1:
+       
+        #ste.st_echarts(key=1,options=option, height="400px")
+        st.plotly_chart(fig, use_container_width=True)
+
+ 
+             
+    with col_report_2:
+        st.write("tarjeta 1")   
+        st.write("tarjeta 2")
+        st.write("tarjeta 3")
+elif selected =="Suspensiones por causa":
+    col1,col2=st.columns(2,gap="small")    
+    with col1:
+
+        STREAMLIT_AGGRID_URL = "https://github.com/PablocFonseca/streamlit-aggrid"
+        st.title("🖱️ Columna 1")
         st.write(
             """ Esta app muestra como puedes usar el componente [streamlit-aggrid](STREAMLIT_AGGRID_URL)
             de Streamlit de forma interactiva de acuerdo a lo que el usuario seleccione."""
@@ -37,7 +87,7 @@ if selected == "Inicio":
             "yAxis": {"type": "value"},
             "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}],
         }
-        ste.st_echarts(key=1,
+        ste.st_echarts(key=2,
             options=option, height="400px")
 
         st.write("Prueba y haz click sobre cualquier fila de la tabla")
@@ -81,65 +131,13 @@ if selected == "Inicio":
 
         st.write("## Code")
 
-        st.code(
-            '''
-        import pandas as pd
-        import streamlit as st
-        from st_aggrid import AgGrid, GridOptionsBuilder
-        from st_aggrid.shared import GridUpdateMode
-
-        iris = pd.read_csv(
-            "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
-        )
-
-        def aggrid_interactive_table(df: pd.DataFrame):
-            """Creates an st-aggrid interactive table based on a dataframe.
-
-            Args:
-                df (pd.DataFrame]): Source dataframe
-
-            Returns:
-                dict: The selected row
-            """
-            options = GridOptionsBuilder.from_dataframe(
-                df, enableRowGroup=True, enableValue=True, enablePivot=True
-            )
-
-            options.configure_side_bar()
-
-            options.configure_selection("single")
-            selection = AgGrid(
-                df,
-                enable_enterprise_modules=True,
-                gridOptions=options.build(),
-                theme="light",
-                update_mode=GridUpdateMode.MODEL_CHANGED,
-                allow_unsafe_jscode=True,
-            )
-
-            return selection
-
-
-        iris = pd.read_csv(
-            "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
-        )
-
-        selection = aggrid_interactive_table(df=iris)
-
-        if selection:
-            st.write("You selected:")
-            st.json(selection["selected_rows"])
-        ''',
-            "python",
-        )
-
     with col2:
 
         STREAMLIT_AGGRID_URL = "https://github.com/PablocFonseca/streamlit-aggrid"
         #st.set_page_config(
         #    layout="centered", page_icon="🖱️", page_title="Interactive table app"   
         #)
-        st.title("🖱️ Aplicacion de Tabla Interactiva")
+        st.title("🖱️ Columna 2")
         st.write(
             """ Esta app muestra como puedes usar el componente [streamlit-aggrid](STREAMLIT_AGGRID_URL)
             de Streamlit de forma interactiva de acuerdo a lo que el usuario seleccione."""
@@ -153,7 +151,7 @@ if selected == "Inicio":
             "yAxis": {"type": "value"},
             "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}],
         }
-        ste.st_echarts(key=2,
+        ste.st_echarts(key=3,
             options=option, height="400px")
         
 else:
